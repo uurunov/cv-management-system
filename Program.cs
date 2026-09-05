cceb6148b52cb514c790f0e162a2b7e613ec313d
uurunov <uurunov.dev@gmail.com>
2026-09-05 18:54:57 +0500

    server reads port from environment variables first

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@ using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
