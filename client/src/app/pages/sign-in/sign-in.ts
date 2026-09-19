06ebb97300f4a2692888f53a464b15806f829162
uurunov <uurunov.dev@gmail.com>
2026-09-19 16:44:12 +0500

    Deploy fix: imported missing module

--- client/src/app/pages/sign-in/sign-in.ts
+++ client/src/app/pages/sign-in/sign-in.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from '@openng/optimus-ui/button';
 
 @Component({
-  imports: [],
+  imports: [ButtonModule],
   selector: 'app-sign-in',
   styleUrl: './sign-in.css',
   templateUrl: './sign-in.html',
